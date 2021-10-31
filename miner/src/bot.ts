93cd36061298df2c85ff5f176fdf3c6e1fc6ec86
Vladislav Yakovlev <vlad309523@gmail.com>
2021-10-31 22:38:14 +0300

    Initial commit

---
+++ miner/src/bot.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+require('dotenv-flow').config()
+
+import axios from 'axios'
+
+(async () => {
+  try {
+    await axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+      params: {
+        chat_id: process.env.CHAT_ID,
+        text   : 'Hello, Telegram!',
+      },
+    })
+  } catch(error) {
+    console.error(error)
+  }
+
+  process.exit(0)
+})()
